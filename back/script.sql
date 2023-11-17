952885ad26d0accffff804d1dd4e7710a848f797
Rémi KORZENIOWSKI <korzeniowski.remi@gmail.com>
2023-11-17 11:43:51 +0100

    fix: SQL Script

--- back/script.sql
+++ back/script.sql
@@ -46,11 +46,11 @@ INSERT INTO Jeu (idJeux, nom, note, prix, descr, image, type)
 VALUES 
 ('1', 'The Witcher 3: Wild Hunt', 4.5, 49.99, 'Action RPG', 'https://thumbnails.pcgamingwiki.com/a/a4/The_Witcher_3_Wild_Hunt_-_cover.jpg/300px-The_Witcher_3_Wild_Hunt_-_cover.jpg', 'Adventure'),
 ('2', 'Minecraft', 4.8, 29.99, 'Open-world sandbox', 'https://productkeys.fi/wp-content/uploads/2021/01/minecraft.jpg', 'Simulation'),
-('3', 'Fortnite', 3.9, 0.00, 'Battle Royale', 'https://cdn-uploads.gameblog.fr/images/jeux/11620/Fortnite_PC_Jaquette_002.jpg?ver=1', 'Action');
-('4', 'Halo 5', 3.8, 24.99, 'Master Chief Adventure', 'https://cdn.vox-cdn.com/thumbor/cSFIh2Z6b6z1Zs3KUdxDjJZqhkA=/59x0:865x537/1400x1400/filters:focal(59x0:865x537):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/46229378/Halo5_KeyArt_Horiz_Final.0.0.jpg', 'FPS');
-('5', 'Forza Horizon 5', 4.2, 35.99, 'Racing adventure', 'https://theouterhaven.b-cdn.net/wp-content/uploads/2021/08/E4HHJZkXMAswau7.jpg', 'Racing');
-('6', 'Sims 4', 4.5, 19.99, 'Create your world', 'https://static.fnac-static.com/multimedia/Images/FR/NR/f8/d1/52/5427704/1507-1/tsp20221215193634/Les-Sims-4-Edition-Limitee-PC.jpg', 'Simulation');
-('7', 'Fifa 22', 3.2, 24.99, 'Live football', 'https://mms.businesswire.com/media/20210711005018/en/890552/4/EAS_FIFA22_GEN5.jpg', 'Sport');
+('3', 'Fortnite', 3.9, 0.00, 'Battle Royale', 'https://cdn-uploads.gameblog.fr/images/jeux/11620/Fortnite_PC_Jaquette_002.jpg?ver=1', 'Action'),
+('4', 'Halo 5', 3.8, 24.99, 'Master Chief Adventure', 'https://cdn.vox-cdn.com/thumbor/cSFIh2Z6b6z1Zs3KUdxDjJZqhkA=/59x0:865x537/1400x1400/filters:focal(59x0:865x537):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/46229378/Halo5_KeyArt_Horiz_Final.0.0.jpg', 'FPS'),
+('5', 'Forza Horizon 5', 4.2, 35.99, 'Racing adventure', 'https://theouterhaven.b-cdn.net/wp-content/uploads/2021/08/E4HHJZkXMAswau7.jpg', 'Racing'),
+('6', 'Sims 4', 4.5, 19.99, 'Create your world', 'https://static.fnac-static.com/multimedia/Images/FR/NR/f8/d1/52/5427704/1507-1/tsp20221215193634/Les-Sims-4-Edition-Limitee-PC.jpg', 'Simulation'),
+('7', 'Fifa 22', 3.2, 24.99, 'Live football', 'https://mms.businesswire.com/media/20210711005018/en/890552/4/EAS_FIFA22_GEN5.jpg', 'Sport'),
 ('8', 'Call Of Duty GHOST', 2.9, 5.99, 'Kill enemies', 'https://static.wikia.nocookie.net/callofduty/images/9/9b/Call_of_Duty_Ghosts_cover.jpg/revision/latest/thumbnail/width/360/height/450?cb=20130501214026', 'FPS');
 
 INSERT INTO Utilisateur (idUser, pseudo, email, pwd)
